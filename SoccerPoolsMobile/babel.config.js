8bb31f19e4ae96db3c86528fd041efbf6cdbcce6
Mati <matiaspendino76@gmail.com>
2025-01-10 15:36:41 +1100

    fix: expo-router lack context fix. update dependencies

--- SoccerPoolsMobile/babel.config.js
+++ SoccerPoolsMobile/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [["inline-dotenv"]]
+    plugins: [
+      '@babel/plugin-transform-export-namespace-from',
+      'babel-plugin-transform-inline-environment-variables',
+      ["inline-dotenv"]
+    ]
   };
 };
