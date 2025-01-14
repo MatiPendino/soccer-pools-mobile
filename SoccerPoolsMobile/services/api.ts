10f055073797b4e0efa94e7a413e1519ffb31428
Mati <matiaspendino76@gmail.com>
2025-01-14 21:16:33 +1100

    fix: API_URL loaded based on environment variable

--- SoccerPoolsMobile/services/api.ts
+++ SoccerPoolsMobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const API_URL: string = 'http://172.20.10.8:8000'
+export const API_URL: string = process.env.API_URL
 
 const api = axios.create({
     baseURL: API_URL
