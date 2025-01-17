0e3bafe9dcd666f8f7228d06d47eeee014aede32
Mati <matiaspendino76@gmail.com>
2025-01-17 19:08:27 +1100

    feat: Sentry integration

--- SoccerPoolsMobile/app/_layout.tsx
+++ SoccerPoolsMobile/app/_layout.tsx
@@ -1,8 +1,12 @@
 import {  Stack } from "expo-router";
 import { ToastProvider } from "react-native-toast-notifications";
 import { StatusBar } from "expo-status-bar";
+import * as Sentry from '@sentry/react-native';
 import '../i18'
 
+Sentry.init({
+    dsn: `https://${process.env.SENTRY_URL}.ingest.us.sentry.io/${process.env.SENTRY_KEY}`,
+})
 
 export default function Layout () {
     return (
