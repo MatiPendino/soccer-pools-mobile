9a3907a600087ed2cb27cd4da518b00d73f172e8
Mati <matiaspendino76@gmail.com>
2025-04-29 11:45:46 +1000

    fix(getNextRoundId): no next round exception solved

--- SoccerPoolsMobile/utils/getNextRound.ts
+++ SoccerPoolsMobile/utils/getNextRound.ts
@@ -6,15 +6,25 @@ export const getNextRoundId = (rounds: RoundProps[]): number => {
         If there is a round on PENDING STATE (equals 1) returns it, otherwise the closest
         round in the future 
     */
-    const currentDate = new Date()
     const pendingRounds = rounds.filter(({round_state}) => round_state === 1)
-    let futureRounds = rounds.filter(({ start_date }) => new Date(start_date) > currentDate)
-    if (futureRounds.length === 0) {
-        futureRounds = rounds.filter(({ round_state }) => round_state === 0)
-    }
-
     if (pendingRounds.length > 0) {
         return pendingRounds[0].id
+    }
+
+    const currentDate = new Date()
+    let futureRounds = rounds.filter(({ start_date }) => new Date(start_date) > currentDate)
+
+    /*
+        If there are no rounds in the future, it is possible that exist rounds which date has not been
+        set yet, so we filter all the NOT STARTED rounds (equals 0).
+        If there are not NOT STARTED rounds, it means that all rounds have already been played
+        and we need to return the id of the last round 
+    */
+    if (futureRounds.length === 0) {
+        futureRounds = rounds.filter(({ round_state }) => round_state === 0)
+        if (futureRounds.length === 0) {
+            return rounds[rounds.length - 1].id
+        }
     }
 
     return futureRounds[0].id
